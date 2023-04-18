c26418ae60a4857361221e6c9f412ddff96c093a
robertw07 <112619961+robertw07@users.noreply.github.com>
2023-04-18 17:25:19 +0800

    update

--- resource_st.go
+++ resource_st.go
@@ -2,7 +2,6 @@ package boomer
 
 import (
 	"fmt"
-	"github.com/prometheus/common/log"
 	"strconv"
 	"time"
 
@@ -56,7 +55,7 @@ type ComputerMonitor struct {
 func GetCPUPercent() float64 {
 	percent, err := cpu.Percent(time.Second, false)
 	if err != nil {
-		log.Error(err.Error())
+		fmt.Println(err.Error())
 		return -1
 	}
 	result, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", percent[0]), 64)
@@ -67,7 +66,7 @@ func GetCPUPercent() float64 {
 func GetMemPercent() float64 {
 	memInfo, err := mem.VirtualMemory()
 	if err != nil {
-		log.Error(err.Error())
+		fmt.Println(err.Error())
 		return -1
 	}
 	result, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", memInfo.UsedPercent), 64)
